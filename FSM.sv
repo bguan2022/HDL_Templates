7e2cc4788eac8a098bbb6540ee75657963ce01c0
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-09-17 16:34:03 -0700

    Update FSM.sv

--- FSM.sv
+++ FSM.sv
@@ -3,14 +3,23 @@
   always @(*) begin 
     case (state)
       IDLE: 
-        if (A)
-          next_state <= S1;
+        if (cond_A)
+          next_state = S1;
+        else
+          next_state = state; 
       S1: 
-        if (B)
-          next_state <= S2;
+        if (cond_B)
+          next_state = S2;
+        else
+          next_state = state;
+      
+      default:
+          next_state = state; 
     endcase
   end 
 
+  assign cond_A = (some_condition) ?  1:0;
+  assign cond_B = (some_condition) ?  1:0;
 
   //move to next state 
   always @(posedge clock or negedge rst_n) begin 
@@ -25,12 +34,13 @@
   always @(*) begin 
     case(state)
       IDLE: 
-        signal_1 = 
+        signal_1 = 1;
 
       S1:
-        singal_2 =
-
+        singal_2 = 0;
+          
       default:
-
+        signal_1 = 0;
+        signal_2 = 0;
      endcase
   end 
